019d9f1b9d401bd31d0293716329caa18798a6fd
Tushar Murarka <tusharmurarka@gmail.com>
2017-04-10 04:00:39 +0530

    Update giftUtility.py

--- Question1/giftUtility.py
+++ Question1/giftUtility.py
@@ -1,11 +1,10 @@
-from Gifts import Gift
 
-class giftUtility(Gift):
+class giftUtility:
 
-    """Gift Class for gifttype = 'Utility'"""
+    utilityClass = None
+    utilityValue = 0
 
     def __init__(self, gift):
-        "constructor"
-        Gift.__init__(self,gift)
-        self.utilityClass = None
-        self.utilityValue = 0
+        self.giftType = gift['giftType']
+        self.price = int(gift['price'])
+        self.value = int(gift['value'])
